9951b38a8c103b1501bc04d4d662a0c3b3e5fd83
javad soltani <j.soltani.it@gmail.com>
2018-07-19 14:01:05 +0430

    comment for test model page

--- app/models/test.php
+++ app/models/test.php
@@ -16,15 +16,19 @@ class test extends jsModel {
      */
     public static function getUsers(){
         $res = '';
-        // jsRequest for get all data such as POST, GET, REQUEST
+        // jsRequest for get all data such as POST, GET, REQUEST and filter the value of this for xss
         //jsRequest::post('id');
         //$status = jsRequest::post('status');
 
+        // selected in database for get all users info
         //$query = static::select(" SELECT id, `name` FROM `".DB_PREFIX."users` WHERE status = '$status' ");
 
+        // get all users
         /*foreach($query as $row){
             $res = "userId = " . $row['id'] . " - userName = " .$row['name'];
         }*/
+
+        // return final data to controller
         return $res;
     }
 
