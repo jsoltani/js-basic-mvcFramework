838d09482609d6ac34e31045b0d0f57e52664795
javad soltani <j.soltani.it@gmail.com>
2018-07-19 13:54:18 +0430

    update test page

--- app/models/test.php
+++ app/models/test.php
@@ -15,17 +15,17 @@ class test extends jsModel {
      * get all information of users
      */
     public static function getUsers(){
-
+        $res = '';
         // jsRequest for get all data such as POST, GET, REQUEST
         //jsRequest::post('id');
         //$status = jsRequest::post('status');
 
         //$query = static::select(" SELECT id, `name` FROM `".DB_PREFIX."users` WHERE status = '$status' ");
-        //$res = '';
+
         /*foreach($query as $row){
             $res = "userId = " . $row['id'] . " - userName = " .$row['name'];
         }*/
-        // return $res;
+        return $res;
     }
 
 }
